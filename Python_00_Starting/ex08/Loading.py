c7f4c73ad6183e022e0a9ce58c2192588c811df8
marwan0620@gmail.com <marwan0620@gmail.com>
2023-03-17 18:38:23 +0100

    D00/ex08 & ex09 finished ---> D00 finished 🎉

---
+++ Python_00_Starting/ex08/Loading.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+
+def ft_tqdm(lst: range) -> None:
+
+    '''\n    Decorate an iterable object, returning an iterator which acts exactly
+    like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested.\n'''
+
+    print("Loading Bar :")
+    for elem in lst:
+
+        ratio = int(elem / len(lst) * 64)
+
+        i = elem / len(lst) * 100
+
+        bar = "=" * int(ratio + 1)
+
+        print(f"\r{i:3.0f}%|[", f"{bar:64}]| {elem + 1}/{len(lst)}", end='', flush = True)
+
+        yield elem
---
+++ Python_00_Starting/ex09/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+
+    name = "ft_package",
+    version = "0.0.1",
+    author = "mayoub",
+    description = "A sample test package",
+    author_email = "mayoub@student.42nice.fr",
+    license = "MIT",
+    url = "https://github.com/Nimpoo/Python-Pool-for-Data-Science.git"
+
+)
