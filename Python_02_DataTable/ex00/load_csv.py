d3e72db91382e2dd80f78dcb46bec4e3d27aa44b
marwan0620@gmail.com <marwan0620@gmail.com>
2023-03-23 14:57:20 +0100

    D02/ex0/1 finished & ex02 started

---
+++ Python_02_DataTable/ex00/load_csv.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.core.frame.DataFrame:
+
+    """
+    /----------- Utility -----------/
+    Load a DataFrame from a .csv file using Pandas.
+
+    /---------- Arguments ----------/
+    path : string
+
+    /----------- Return ------------/
+    Return a DataFrame .csv from the path in argument
+    Return None when encounter an Error
+    """
+
+    try:
+        data_frame = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {data_frame.shape}")
+
+    except FileNotFoundError:
+        print("FileNotFoundError: pls use a valid file")
+        return (None)
+    except ValueError:
+        print("ValueError: pls use a string type to set a valid path")
+        return (None)
+    except Exception as e:
+        print(e)
+        return (None)
+
+    return (data_frame)
---
+++ Python_02_DataTable/ex02/aff_pop.py
@@ -0,0 +1,22 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    """
+    /----------- Utility -----------/
+    
+
+    /---------- Arguments ----------/
+    
+
+    /----------- Return ------------/
+    
+    """
+
+    
+
+
+if __name__ == "__main__":
+    main()
